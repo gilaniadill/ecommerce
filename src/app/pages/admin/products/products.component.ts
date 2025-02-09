3521b3e976a16a7b001604187822846c068d89e8
adil gilani <adilgilani94@gmail.com>
2025-02-09 10:46:44 +0500

    created input form to add products

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
+  isNewCardVisible: boolean = false;
 
+  productObj:any={
+    
+      "productId": 0,
+      "productSku": "",
+      "productName": "",
+      "productPrice": 0,
+      "productShortName": "",
+      "productDescription": "",
+      "createdDate": new Date(),
+      "deliveryTimeSpan": "",
+      "categoryId": 0,
+      "productImageUrl": "",
+      "userId": 0
+  
+  }
+
+  showNewCard(){
+    this.isNewCardVisible = true;
+  }
+  closeNewCard(){
+    this.isNewCardVisible = false;
+  }
 }
